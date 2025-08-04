0149d1fce6b2c526ddefb21d6eaf0c4705d5ccdf
harshiltomar <harshiltomar20@gmail.com>
2025-08-04 14:08:14 +0530

    feat: header setup

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,47 @@
-import React from 'react'
+"use client";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { LogIn, Shield } from "lucide-react";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { usePathname } from "next/navigation";
 
 function Header() {
+
+  const pathname = usePathname();
+
+  const isHomePage = pathname === "/";
+
   return (
-    <div className='text-4xl font-bold text-center'>
-        Header
+    <div className={`p-4 flex justify-between items-center ${isHomePage ? "bg-blue-50" : "bg-white border-b border-blue-100"}`}>
+      <Link href="/" className="flex items-center">
+        <Shield className="w-6 h-6 text-blue-600 mr-2" />
+
+        <h1 className="text-xl font-semibold">Money Logs</h1>
+      </Link>
+
+      <div className="flex items-center space-x-4">
+        <SignedIn>
+          <Link href="/receipts">
+            <Button variant="outline">My reciepts</Button>
+          </Link>
+
+          <Link href="/manage-plan">
+            <Button>Manage Plan</Button>
+          </Link>
+          <UserButton />
+        </SignedIn>
+
+        <SignedOut>
+          <SignInButton mode="modal">
+            <Button>
+              <LogIn className="w-4 h-4 mr-2" />
+              Login
+            </Button>
+          </SignInButton>
+        </SignedOut>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
