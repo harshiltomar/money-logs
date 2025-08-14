f565e5816afc33f7b59c00d5add361e7b31a0ea9
harshiltomar <harshiltomar20@gmail.com>
2025-08-14 13:03:30 +0530

    chore: schematic init

---
+++ actions/get-temorary-access-token.ts
@@ -0,0 +1,29 @@
+'use server'
+
+import { currentUser } from "@clerk/nextjs/server"
+// Initialize Schematic SDK
+import { SchematicClient } from "@schematichq/schematic-typescript-node";
+const apiKey = process.env.SCHEMATIC_API_KEY;
+const client = new SchematicClient({ apiKey });
+
+// Get a temporary access token
+export async function getTemporaryAccessToken() {
+    console.log("Getting Temporary access token");
+    const user = await currentUser();
+
+    if (!user) {
+        console.log("No user found");
+        return null;
+    }
+
+    console.log(`User found. Issuing temporary access token for user ${user.id}`);
+
+    const resp = await client.accesstokens.issueTemporaryAccessToken({
+        //resourceType: "company",
+        lookup: { userId: user.id }, // The lookup will vary depending on how you have configured your company keys
+    });
+
+    console.log(`Temporary access token issued for user ${user.id}. Token: ${resp.data?.token}`);
+
+    return resp.data?.token;
+}
