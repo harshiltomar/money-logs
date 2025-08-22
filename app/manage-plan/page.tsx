e1e78e285015cdc7f137fecbbac86785ca61b7ea
harshiltomar <harshiltomar20@gmail.com>
2025-08-22 13:06:19 +0530

    feat: inngest init

--- app/manage-plan/page.tsx
+++ app/manage-plan/page.tsx
@@ -2,10 +2,17 @@ import SchematicComponent from '@/components/schematic/schematic-component'
 
 export default function page() {
   return (
-    <div>
-        <SchematicComponent
-            componentId={process.env.NEXT_PUBLIC_SCHEMATIC_CUSTOMER_PORTAL_COMPONENT_ID!}
-        />
+    <div className="container xl:max-w-5xl mx-auto p-4 md:p-0">
+      <h1 className="text-2xl font-bold mb-4 my-8">Manage Your Plan</h1>
+      <p className="text-gray-600 mb-8">
+        Manage your subscription and billing details here.
+      </p>
+
+      <SchematicComponent
+        componentId={
+          process.env.NEXT_PUBLIC_SCHEMATIC_CUSTOMER_PORTAL_COMPONENT_ID
+        }
+      />
     </div>
   )
 }
